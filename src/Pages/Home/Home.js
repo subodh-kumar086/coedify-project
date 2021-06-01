1a8a390851beaafecf63bff9ad8980f0efc2fdd2
SUBODH KUMAR <subodh.kumar086@gmail.com>
2021-06-01 23:16:32 +0530

    crousel intialised

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
-
+import "./style.css"
 import Navbar from '../../components/Navbar/Navbar';
 import Data from '../../components/Data/Data';
 import Header from '../../components/Header/Header';
+import Crousel from '../../components/Crousel/Crousel';
 
 
 function Home() {
     return (
         <>
+
             <Navbar/>
             <Header/>
             <Data/>
+            <hr/>
+            <Crousel/>
+
             
         </>
     )
---
+++ src/components/Crousel/CrouselData.jsx
@@ -0,0 +1,28 @@
+import crimg1 from "./images/split-image-01.png";
+import crimg2 from "./images/split-image-02.png";
+import crimg3 from "./images/split-image-03.png";
+
+
+const CrouselData = [
+    {
+        id : 1,
+        imgSrc : crimg1,
+        title : "Robust Workflow1Data-driven insights",
+        Body :  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua — Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat."
+    },
+    {
+        id : 2,
+        imgSrc : crimg2,
+        title : "Robust Workflow1Data-driven insights",
+        Body :  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua — Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat."
+    },
+    {
+        id : 3,
+        imgSrc : crimg3,
+        title : "Robust Workflow1Data-driven insights",
+        Body :  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua — Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat."
+    }
+    
+];
+
+export default CrouselData;
